da842b2d72ddd98aa38378d818c332ced3bfe701
Alexandr Dryagin <alexandrdryagin@yandex.ru>
2020-10-09 16:59:54 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split("");
+    if (arr.length % 2 !== 0) return false;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                arr[i] === bracketsConfig[j][0] &&
+                arr[i + 1] === bracketsConfig[j][1]
+            ) {
+                arr.splice(i, 2);
+                i = -1;
+            } else {
+                continue;
+            }
+        }
+    }
+    return arr.length === 0 ? true : false;
 }
